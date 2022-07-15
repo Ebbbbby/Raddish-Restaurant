f09e827d0230382cac28c3498c49f30a7a11e9f1
ebbbbby <ocynthiaebele@gmail.com>
2022-07-15 09:22:33 +0100

    Identity server config

--- Raddish.Services.Identity/DbContexts/ApplicationDbContext.cs
+++ Raddish.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Raddish.Services.Identity.Models;
 
 namespace Raddish.Services.Identity.DbContexts
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
